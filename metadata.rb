1d37bcb4503b8d14d4d93af7347bdf6ec6467c85
davisc <clarence.davis@tech.geocent.com>
2013-03-27 14:04:13 -0400

    Downloads OpenAM war and tools. Deploys OpenAM and executes the sso config toos.
    Requires Tomcat

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,17 @@ license          "All rights reserved"
 description      "Installs/Configures OpenAM Access Management Platform"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+
+%w{ java }.each do |cb|
+  depends cb
+end
+
+%w{ debian ubuntu }.each do |os|
+  supports os
+end
+
+recipe "tomcat-openam::full_stack", "Installs and configures Tomcat, OpenDJ and OpenAM In a single instance"
+recipe "tomcat-openam::default", "Installs Tomcat and OpenAM. But Configures only tomcat"
+recipe "tomcat-openam::configure", "Configures OpenAM" #It needs a dns of an instance which is running opendj
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,9 +1,104 @@
 #
-# Cookbook Name:: geoserver_demo
+# Cookbook Name:: OpenAM
 # Recipe:: default
 #
 # Copyright 2012, Geocent
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "apache2"
+include_recipe "apache2::mod_proxy"
+include_recipe "apache2::mod_proxy_http"
+
+case node[:platform]
+when "debian", "ubuntu"
+  
+
+remote_file node["tomcat-openam"]["remote-location"]["openam-tar"] do
+  source node["tomcat-openam"]["source"]["openam-war"]
+  mode node["tomcat-openam"]["mode"]
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  checksum node["tomcat-openam"]["checksum"] 
+end
+
+directory node["tomcat-openam"]["dir-path"]["tmp"] do
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+  action :create
+end
+
+directory node["tomcat-openam"]["dir-path"]["openam"] do
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+  action :create
+end
+
+directory node["tomcat-openam"]["dir-path"]["tmp-openam"] do
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+  action :create
+end
+
+
+remote_file node["tomcat-openam"]["remote-location"]["ssoConfigurator"] do
+  source node["tomcat-openam"]["source"]["ssoconfig-zip"]
+  mode node["tomcat-openam"]["mode"]
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  checksum node["tomcat-openam"]["checksum"] 
+end
+
+package 'zip'
+execute "unzip openam ssoconfigurator" do
+  command <<CMD
+umask 022
+unzip -u -o "/home/vagrant/tmp/openam/ssoConfiguratorTools.zip"
+CMD
+  cwd node["tomcat-openam"]["dir-path"]["tmp-openam"]
+  user node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  action :run
+  not_if { ::File.exist?("/home/vagrant/tmp/openam/configurator.jar") }
+end
+
+#DAV
+
+template node["tomcat-openam"]["remote-location"]["openam-config"] do
+  source node["tomcat-openam"]["template"]["openam-config"]
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+ end
+
+directory node["tomcat-openam"]["dir-path"][".openssocfg"] do
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+  action :create
+end
+
+directory node["tomcat-openam"]["dir-path"]["openam"] do
+  owner node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  mode node["tomcat-openam"]["mode"]
+  action :create
+end
+
+execute "configuring-openam" do
+  cwd node["tomcat-openam"]["dir-path"][".openssocfg"]
+  user node["tomcat-openam"]["user"]
+  group node["tomcat-openam"]["user"]
+  command node["tomcat-openam"]["cmd"]["config-sso"]
+   action :run
+end
+
+end
+
+
+
 
